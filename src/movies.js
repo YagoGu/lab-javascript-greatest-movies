df85f74b8d9eaf7c5bded4fbc80a5f64b330c98d
YagoGu <yago.gutierrez15@gmail.com>
2023-07-17 22:33:17 +0200

    i think it's correct

--- src/movies.js
+++ src/movies.js
@@ -95,4 +95,6 @@ function turnHoursToMinutes(moviesArray) {
 }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg(moviesArray) {}
+function bestYearAvg(moviesArray) {
+    s
+}
